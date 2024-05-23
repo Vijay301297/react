fb11faa27c3b1ee69c85c648e9f28b91c953833b
Vijay301297 <159887297+Vijay301297@users.noreply.github.com>
2024-05-23 21:12:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,45 @@
-export function App() {
-  return <h1>Hi</h1>;
+import { useState } from "react";
+
+const messages  = [
+    'Learning React is good',
+    'Learning Django is very good',
+    'Larning DSA is ultimate'
+]
+
+
+export default function App() {
+    
+    const [step, stepIncrement] = useState(1);
+    const [openModal, closeModal] = useState(true);
+
+    function previousHandler() {
+        if (step>1) stepIncrement((curStep) => curStep - 1);
+    }
+
+    function nextHandler() {
+        if (step<3) stepIncrement((curStep) => curStep + 1);
+        // stepIncrement(step+1);
+    }
+
+    return (
+        <>
+        <button className="close" onClick={() => closeModal((om) => !om )}>&times;</button>
+        {
+            openModal && (
+                <div className="steps">
+                    <div className="numbers">
+                        <div className={step >=1 ? 'active': ''}>1</div>
+                        <div className={step >=2 ? 'active': ''}>2</div>
+                        <div className={step >=3 ? 'active': ''}>3</div>
+                    </div>
+                    <p className="message">Step {step} : {messages[step-1]}</p>
+                    <div className="buttons">
+                        <button style={{background: '#7950f2', color:'#fff'}} onClick={previousHandler}>Previous</button>
+                        <button style={{background: '#7950f2', color:'#fff'}} onClick={nextHandler}>Next</button>
+                    </div>
+                </div>
+            )
+        }
+        </>
+    );
 }
